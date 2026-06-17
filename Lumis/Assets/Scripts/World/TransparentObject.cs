769676cb6590e4ba4d919dcb5089277635d33063
Mila Milovic <milamilovic03@gmail.com>
2026-06-18 00:58:05 +0200

    base station sprites + transparency and collision

--- Lumis/Assets/Scripts/World/TransparentObject.cs
+++ Lumis/Assets/Scripts/World/TransparentObject.cs
@@ -7,35 +7,37 @@ public class TransparentObject : MonoBehaviour
 
     private SpriteRenderer sr;
     private Transform player;
-    private float targetAlpha = 1f;
 
-    private Collider2D col;
+    private float targetAlpha = 1f;
 
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
-        player = GameObject.FindGameObjectWithTag("Player").transform;
-        col = GetComponent<Collider2D>();
+
+        GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
+        if (playerObj != null)
+            player = playerObj.transform;
     }
 
     void Update()
     {
-        if (player == null) return;
+        if (player == null)
+            return;
 
-        float spriteHeight = sr.bounds.size.y;
-        float spriteWidth = sr.bounds.size.x;
-
-        float objectBase = transform.position.y - spriteHeight * 0.33f;
-
-        bool playerIsBehind = player.position.y > objectBase && player.position.y < objectBase + spriteHeight * 1.5;
-        bool xOverlap = Mathf.Abs(player.position.x - transform.position.x) < spriteWidth * 0.5f;
-
-        targetAlpha = (xOverlap && playerIsBehind) ? transparencyAmount : 1f;
-
+        // Fade
         Color c = sr.color;
         c.a = Mathf.Lerp(c.a, targetAlpha, fadeSpeed * Time.deltaTime);
         sr.color = c;
 
-        sr.sortingOrder = playerIsBehind ? 9999 : -500;
+        // Sorting
+        sr.sortingOrder =
+            player.position.y > transform.position.y
+            ? 9999
+            : -500;
+    }
+
+    public void SetTransparent(bool transparent)
+    {
+        targetAlpha = transparent ? transparencyAmount : 1f;
     }
 }
--- README.md
+++ README.md
@@ -70,8 +70,8 @@ License: Attribution 4.0
 
 
 
-Tone12\\\_Success 
-Author: ScottyD0ES 
-ULR: https://freesound.org/s/716450/ 
+Tone12\\\_Success
+Author: ScottyD0ES
+ULR: https://freesound.org/s/716450/
 License: Attribution NonCommercial 4.0
 
